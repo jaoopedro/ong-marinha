57e7dd42e54f4d08abef9faa15bd316d10a50d5c
joao pedro <87543850+jaoopedro@users.noreply.github.com>
2022-05-02 10:36:06 -0300

    Acrescentando alert

--- script.js
+++ script.js
@@ -2,5 +2,9 @@
 function onClickMenu(){
 	document.getElementById("menu").classList.toggle("change");
 	document.getElementById("nav").classList.toggle("change");
-	document.getElementById("menu-bg").classList.toggle("change-bg")
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+function aba(){
+	alert("Ainda não é Possivel fazer doações 😭");
 }
